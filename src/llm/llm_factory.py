ac2afc088ca0e7c6e2d33e04000bef79af5b7908
tdu7 <126829455+tdu7@users.noreply.github.com>
2025-04-22 22:54:19 +0200

    Implémentation de la LLM Factory : Pleiade/Openai

--- src/llm/llm_factory.py
+++ src/llm/llm_factory.py
@@ -1,90 +1,81 @@
+from typing import List, Dict, Any
 from pydantic import BaseModel
+from llms.pleiade import Pleiade
+from llms.openai_client import OpenAI
 
 
-class Country(BaseModel):
-    name: str
-    capital: str
-    languages: list[str]
+class DatasetStructureItem(BaseModel):
+    new_column_name: str
+    source_columns: List[str]
+    column_description: str
+    transformation_type: str
+    # Peut être précisé selon le type attendu
+    transformation_params: Dict[str, Any]
+
+
+class ResponseFormat(BaseModel):
+    dataset_id: str
+    # dataset_source_file: str
+    prompt: str  # Non retourner pas le LLM
+    target_column: str
+    # configs: config
+    dataset_structure: List[DatasetStructureItem]
 
 
 class LLMFactory:
 
     @staticmethod
     def create_llm(config: dict):
-        import requests
-        import json
         provider = config["provider"]
         if provider == "Pleiade":
-            from llms.pleiade import Pleiade
-
-            api_url = Pleiade.get_api_url()
-            api_key = Pleiade.get_api_key()
-
             Pleiade.set_model(config["model"])
-            model = Pleiade.get_model()
-            prompt = config.get("prompt")
+            return Pleiade
 
-            request_body = {
-                "model": model,
-                "messages": [{
-                    "role": "user",
-                    "content": prompt
-                }],
-                "format": Country.model_json_schema()
-            }
-            headers = {
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {api_key}"
-            }
-
-            response = requests.post(
-                api_url,
-                headers=headers,
-                data=json.dumps(request_body)
-            )
-            json_formatted = json.dumps(response.json(), indent=4)
-            print(json_formatted)
-
-        elif provider == "OpenAI":  # Notez le 'elif' ici au lieu d'un nouveau 'if'
-            from llama_index.llms.openai import OpenAI
-            from llms.openai import OpenAI
-            api_url = OpenAI.get_api_url()
-            api_key = OpenAI.get_api_key()
-            model = OpenAI.get_model()
-
-            return OpenAI(
-                model=model,
-                api_key=api_key
-            )
+        elif provider == "OpenAi":
+            OpenAI.set_model(config["model"])
+            return OpenAI
         else:
             raise ValueError(f"LLM provider {provider} is not supported.")
 
 
-config = {
+# POUR TESTER OPEN AI LA REPONSE DOIT ETRE LA CLASSE
+'''config = {
+    "provider": "OpenAi",
+    "model": "dall-e-3",
+    "prompt": """
+    Tu es expert en feature engineering et voici un ensemble de données sous format JSON. 
+    Les colonnes disponibles sont : taille, poids, âge, masse, sexe. 
+    À partir de ces données, ta tâche est de créer de nouvelles colonnrd pertinentes. Pour savoir si les personnes sont atteintes d'obésité.
+    Donne des exemples de transformations, de calculs ou d'agrégations sur ces données.
+
+    Données : 
+    [
+      { "taille": 175, "poids": 70, "âge": 28, "masse": 22.9, "sexe": "H" },
+      { "taille": 162, "poids": 60, "âge": 34, "masse": 22.9, "sexe": "F" },
+      { "taille": 180, "poids": 85, "âge": 45, "masse": 26.2, "sexe": "H" }
+    ]
+    """
+}'''
+
+# POUR TESTER PLEIADE RENVOIE <class 'llms.pleiade.Pleiade'>
+'''config = {
     "provider": "Pleiade",
     "model": "llama3.3:latest",
-    "prompt": "Tell me about Canada."
-}
+    "prompt": """
+    Tu es expert en feature engineering et voici un ensemble de données sous format JSON. 
+    Les colonnes disponibles sont : taille, poids, âge, masse, sexe. 
+    À partir de ces données, ta tâche est de créer de nouvelles colonnrd pertinentes. Pour savoir si les personnes sont atteintes d'obésité.
+    Donne des exemples de transformations, de calculs ou d'agrégations sur ces données.
+
+    Données : 
+    [
+      { "taille": 175, "poids": 70, "âge": 28, "masse": 22.9, "sexe": "H" },
+      { "taille": 162, "poids": 60, "âge": 34, "masse": 22.9, "sexe": "F" },
+      { "taille": 180, "poids": 85, "âge": 45, "masse": 26.2, "sexe": "H" }
+    ]
+    """
+}'''
 
 reponse = LLMFactory.create_llm(config)
 
-
-# CIBLE
-"""             return Pleiade(
-                model=config["model"],
-            ) """
-
-# CIBLE
-config = {
-    "provider": "Pleiade",
-    "model": "llama3.3:latest",
-}
-
-
-# CIBLE DANS FE
-
-
-class Country(BaseModel):
-    name: str
-    capital: str
-    languages: list[str]
+print(reponse)
--- src/llm/llms/pleiade.py
+++ src/llm/llms/pleiade.py
@@ -1,3 +1,22 @@
+from typing import List, Dict, Any, Optional, Type
+from pydantic import BaseModel
+import requests
+import json
+
+
+class DatasetStructureItem(BaseModel):
+    new_column_name: str
+    source_columns: List[str]
+    column_description: str
+    transformation_type: str
+    # Peut être précisé selon le type attendu
+    transformation_params: Dict[str, Any]
+
+
+class ResponseFormat(BaseModel):
+    dataset_structure: List[DatasetStructureItem]
+
+
 class Pleiade:
     _available_models = [
         "athene-v2:latest",
@@ -19,8 +38,8 @@ class Pleiade:
         "qwen2-math:72b"
     ]
 
-   # mettre un model par défaut  _model =
-    _api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcxMTEwNzM3LTA1ZmMtNDhlNS05MDg5LTllOTI0MjFkZDFmZiJ9.07UkYLz-0rTcwIG9Rkwr8Ql7_JqoFaO3OtUSKiMDkJ0"
+    _model = "llama3.3:latest"
+    _api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcxMTEwNzM3LTA1ZmMtNDhlNS05MDg5LTllOTI0MjFkZDFmZiJ9.1vTBTG7H1TlSYuXnDPqvwoObzO2FmGHZGlJ4tiOTC7I"
     _api_url = "https://pleiade.mi.parisdescartes.fr/api/chat/completions"
 
     @classmethod
@@ -48,6 +67,58 @@ class Pleiade:
     def get_api_url(cls):
         return cls._api_url
 
+    @classmethod
+    def call(cls, prompt: str, response_format: Optional[Type[BaseModel]] = None):
+        request_body = {
+            "model": cls.get_model(),
+            "messages": [{"role": "user", "content": prompt}]
+        }
 
-# faire une fonction call avec les attributs call( prompt, response format)
-# Avec cette archi , c'est simple de tester plusieurs llm pour le FE
+        if response_format:
+            request_body["format"] = response_format.model_json_schema()
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {cls.get_api_key()}"
+        }
+
+        response = requests.post(
+            cls.get_api_url(),
+            headers=headers,
+            data=json.dumps(request_body)
+        )
+
+        try:
+            response.raise_for_status()
+            response_json = response.json()
+            print(json.dumps(response_json, indent=4))
+            return response_json
+        except requests.exceptions.RequestException as e:
+            print(f"Erreur : {e}")
+            print(response.text)
+            return None
+
+
+# voir branch de yvonne pour les co avec les transfo
+# nommage branche feature/LLM num ticket
+config = {
+    "provider": "Pleiade",
+    "model": "llama3.3:latest",
+    "prompt": """
+    Tu es expert en feature engineering et voici un ensemble de données sous format JSON. 
+    Les colonnes disponibles sont : taille, poids, âge, masse, sexe. 
+    À partir de ces données, ta tâche est de créer de nouvelles colonnrd pertinentes. Pour savoir si les personnes sont atteintes d'obésité.
+    Donne des exemples de transformations, de calculs ou d'agrégations sur ces données.
+
+    Données : 
+    [
+      { "taille": 175, "poids": 70, "âge": 28, "masse": 22.9, "sexe": "H" },
+      { "taille": 162, "poids": 60, "âge": 34, "masse": 22.9, "sexe": "F" },
+      { "taille": 180, "poids": 85, "âge": 45, "masse": 26.2, "sexe": "H" }
+    ]
+    """
+}
+
+"""
+Pleiade.call(config["prompt"])
+Pleiade.call(config["prompt"], response_format=ResponseFormat)"""
