ec640b2204f2323397b04479a9baad0ea1c05f91
Nicolas L <nicolas.loisy@yahoo.fr>
2025-05-22 01:40:50 +0200

    Refactor logging configuration and update run script: change logger qualname to 'src' and adjust logging path

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
-
 if __name__ == "__main__":
+    import os
     from pathlib import Path
     from src.orchestrator.orchestrator import Orchestrator
     from src.utils.logger import init_logger
-
-    # Logging initialization
-    logging_path = Path(__file__).parent / "logging.ini"
-    init_logger(logger_path=logging_path)
+    import pprint
+    
+    logging_path = os.path.join(Path(__file__).parent, "data", "logs","logging.ini")
+    init_logger(logger_path=str(logging_path))
 
     orchestrator = Orchestrator(config_path="data/config.json")
-    new_dataset = orchestrator.run(dataset_path="data/datasets/data.csv")
-    print(new_dataset.head())
+
+    description = "This is a sample dataset with various features of health data and other, the target is the 'status' column."
+
+    new_dataset = orchestrator.run(dataset_path="data/datasets/data.csv", dataset_description=description, target_column="status", iterations=1)
+    pprint.pprint(new_dataset)
