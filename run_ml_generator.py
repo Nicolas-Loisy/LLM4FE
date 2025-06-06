233244aeb78cb1995500d67768c122da59bed56e
Diot Clément <clement.diot.jr@gmail.com>
2025-06-06 10:52:21 +0200

    feat: add the metric management for ml_generator

--- run_ml_generator.py
+++ run_ml_generator.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
-from src.utils.logger import init_logger
 from src.ml_generator.ml_estimator import MachineLearningEstimator
 
 if __name__ == "__main__":
     ml_estimator = MachineLearningEstimator(dataset_path=Path("data/tests/data.csv"), target_col="target")
     ml_score = ml_estimator.run()
     print(ml_score)
+    ml_better = ml_estimator.score_better(2340)
+    print(ml_better)
 
 
 
